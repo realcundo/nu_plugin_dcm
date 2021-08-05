d37a97a99a4cb221f91104f8ec1fe6dd311aebd1
Peter Cunderlik <realcundo@users.noreply.github.com>
2021-08-05 17:16:00 +0100

    Add tests to read DCM files with preambles

---
+++ tests/assets/generate-files.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from pydicom.dataset import FileDataset, FileMetaDataset
+import pydicom
+import os
+
+ASSETS_DIR = os.path.dirname(__file__)
+
+
+class ExplicitVRLittleEndian:
+    uid = pydicom.uid.ExplicitVRLittleEndian
+    little_endian = True
+    implicit_vr = False
+
+
+class ImplicitVRLittleEndian:
+    uid = pydicom.uid.ImplicitVRLittleEndian
+    little_endian = True
+    implicit_vr = True
+
+
+class ExplicitVRBigEndian:
+    uid = pydicom.uid.ExplicitVRBigEndian
+    little_endian = False
+    implicit_vr = False
+
+
+class DeflatedExplicitVRLittleEndian:
+    uid = pydicom.uid.DeflatedExplicitVRLittleEndian
+    little_endian = True
+    implicit_vr = False
+
+
+for transfer_syntax in [
+    ExplicitVRLittleEndian,
+    ImplicitVRLittleEndian,
+    ExplicitVRBigEndian,
+    # DeflatedExplicitVRLittleEndian, # TODO
+]:
+    for preamble in [True]:
+        suffix = "Preamble" if preamble else "NoPreamble"
+        name = f"{transfer_syntax.__name__}-{suffix}"
+        filename = f"{name}.dcm"
+
+        file_meta = FileMetaDataset()
+        file_meta.MediaStorageSOPClassUID = "1.2.840.10008.5.1.4.1.1.2"
+        file_meta.MediaStorageSOPInstanceUID = "1.2.3"
+        file_meta.ImplementationClassUID = "1.2.3.4"
+        file_meta.FileMetaInformationGroupLength = 0  # will be updated
+
+        preamble_data = b"\0" * (128 if preamble else 0)
+
+        ds = FileDataset(filename, {}, file_meta=file_meta, preamble=preamble_data)
+        ds.file_meta.TransferSyntaxUID = transfer_syntax.uid
+        ds.is_little_endian = transfer_syntax.little_endian
+        ds.is_implicit_VR = transfer_syntax.implicit_vr
+
+        ds.PatientName = name
+
+        ds.save_as(os.path.join(ASSETS_DIR, filename))
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,14 +1,29 @@
 #[macro_use]
 extern crate assert_matches;
 
+use std::path::PathBuf;
+
 use nu_plugin::Plugin;
 use nu_plugin_dcm::plugin::DcmPlugin;
 
-use nu_protocol::ReturnSuccess;
-use nu_test_support::value::nothing;
+use nu_protocol::{ReturnSuccess, UntaggedValue, Value};
+use nu_test_support::value::{nothing, row, string};
+
+use indexmap::indexmap;
+
+fn get_asset_filename(filename: &str) -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join("assets")
+        .join(filename)
+}
+
+pub fn filepath(input: impl Into<PathBuf>) -> Value {
+    UntaggedValue::filepath(input.into()).into_untagged_value()
+}
 
 #[test]
-fn test_no_input_without_errors() {
+fn no_input_without_errors() {
     let mut p = DcmPlugin::default();
     let actual = p.filter(nothing());
 
@@ -17,7 +32,7 @@ fn test_no_input_without_errors() {
 }
 
 #[test]
-fn test_no_input_with_silent_errors() {
+fn no_input_with_silent_errors() {
     let mut p = DcmPlugin {
         silent_errors: true,
         ..Default::default()
@@ -26,5 +41,65 @@ fn test_no_input_with_silent_errors() {
     let actual = p.filter(nothing());
 
     // TODO can't compare directly because ReturnSuccess doesn't impl == yet.
-    assert_matches!(&actual.unwrap()[..], [Ok(ReturnSuccess::Value(expected))] if expected == &nothing());
+    assert_matches!(&actual.unwrap()[..], [Ok(ReturnSuccess::Value(actual_value))] if actual_value == &nothing());
+}
+
+#[test]
+fn read_explicit_vr_big_endian_preamble() {
+    let filename = get_asset_filename("ExplicitVRBigEndian-Preamble.dcm");
+
+    let mut p = DcmPlugin::default();
+    let actual = p.filter(filepath(filename));
+
+    let expected = row(indexmap! {
+        "TransferSyntax".to_string() => string("1.2.840.10008.1.2.2"),
+        "MediaStorageSOPClassUID".to_string() => string("1.2.840.10008.5.1.4.1.1.2"),
+        "MediaStorageSOPInstanceUID".to_string() => string("1.2.3"),
+        "PatientName".to_string() => string("ExplicitVRBigEndian-Preamble"),
+
+    });
+
+    // TODO can't compare directly because ReturnSuccess doesn't impl == yet.
+    let actual = actual.unwrap()[0].as_ref().unwrap().raw_value().unwrap();
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn read_explicit_vr_little_endian_preamble() {
+    let filename = get_asset_filename("ExplicitVRLittleEndian-Preamble.dcm");
+
+    let mut p = DcmPlugin::default();
+    let actual = p.filter(filepath(filename));
+
+    let expected = row(indexmap! {
+        "TransferSyntax".to_string() => string("1.2.840.10008.1.2.1"),
+        "MediaStorageSOPClassUID".to_string() => string("1.2.840.10008.5.1.4.1.1.2"),
+        "MediaStorageSOPInstanceUID".to_string() => string("1.2.3"),
+        "PatientName".to_string() => string("ExplicitVRLittleEndian-Preamble"),
+
+    });
+
+    // TODO can't compare directly because ReturnSuccess doesn't impl == yet.
+    let actual = actual.unwrap()[0].as_ref().unwrap().raw_value().unwrap();
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn read_implicit_vr_little_endian_preamble() {
+    let filename = get_asset_filename("ImplicitVRLittleEndian-Preamble.dcm");
+
+    let mut p = DcmPlugin::default();
+    let actual = p.filter(filepath(filename));
+
+    let expected = row(indexmap! {
+        "TransferSyntax".to_string() => string("1.2.840.10008.1.2"),
+        "MediaStorageSOPClassUID".to_string() => string("1.2.840.10008.5.1.4.1.1.2"),
+        "MediaStorageSOPInstanceUID".to_string() => string("1.2.3"),
+        "PatientName".to_string() => string("ImplicitVRLittleEndian-Preamble"),
+
+    });
+
+    // TODO can't compare directly because ReturnSuccess doesn't impl == yet.
+    let actual = actual.unwrap()[0].as_ref().unwrap().raw_value().unwrap();
+    assert_eq!(actual, expected);
 }
