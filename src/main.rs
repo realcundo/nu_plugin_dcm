3bf5f83d6448813c6cf7132a2c6fef59248ceec0
Eduardo Pinho <enet4mikeenet@gmail.com>
2025-06-16 18:11:11 +0100

    Prefer MgsPackSerializer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use nu_plugin::JsonSerializer;
+use nu_plugin::MsgPackSerializer;
 
 mod convert;
 mod dcm;
@@ -11,6 +11,5 @@ fn main() {
 
     // echo $files | merge { echo $files.name | dcm | get data | select Modality PixelSpacing.0 PixelSpacing.1 } | sort-by Modality name
 
-    // use JsonSerializer because Cap'nProto doesn't support CellPath yet (https://github.com/nushell/nushell/issues/5023 and https://github.com/nushell/nushell/pull/4920)
-    nu_plugin::serve_plugin(&mut plugin, JsonSerializer {});
+    nu_plugin::serve_plugin(&mut plugin, MsgPackSerializer);
 }
