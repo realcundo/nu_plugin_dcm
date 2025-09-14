c9886c1607b968be4b7c36f3a541b391e6249d55
Peter Cunderlik <realcundo@users.noreply.github.com>
2025-09-14 21:07:12 +0100

    Filter out nu::Span from insta snapshot to reduce noise

--- tests/dcm_parser_insta.rs
+++ tests/dcm_parser_insta.rs
@@ -10,6 +10,18 @@ fn private_assets() -> Result<(), nu_protocol::ShellError> {
     let mut plugin_test = setup_plugin_for_test(vec![Box::new(nu_command::Open)])?;
     let asset_path = get_asset_base_path().join("private");
 
+    let insta_filters = vec![
+        // replace nu::Span with empty string:
+        //  - `(?ms)` turns on multiline+ dot matches newline
+        //  - .*? makes sure it's non-greedy
+        // - example:
+        //     span: Span(
+        //       start: 0,
+        //       end: 4,
+        //     ),
+        (r"(?ms)^\s*span: Span\(.*?\),.^", ""),
+    ];
+
     glob!(asset_path, "**/*.dcm", |path| {
         let command = format!("open --raw {} | dcm", path.to_string_lossy());
 
@@ -19,7 +31,7 @@ fn private_assets() -> Result<(), nu_protocol::ShellError> {
             .into_value(Span::test_data())
             .expect("failed to convert to value");
 
-        insta::with_settings!({sort_maps => true, snapshot_path => "snapshots/private" }, {
+        insta::with_settings!({sort_maps => true, snapshot_path => "snapshots/private", filters=>insta_filters.clone()}, {
             assert_ron_snapshot!(result);
         });
     });
